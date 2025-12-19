aa528b1ca3ebd2a562132ac1122a7f35bdf1b0ac
trentquacks <kingofduq@gmail.com>
2025-12-19 13:31:26 +0800

    scale

--- home/bin/hyprland_eco.sh
+++ home/bin/hyprland_eco.sh
@@ -2,12 +2,12 @@ if [[ "$1" == on ]] ; then
 	hyprctl keyword decoration:blur:enabled false
 	hyprctl keyword decoration:shadow:enabled false
 	hyprctl keyword animations:enabled false
-	hyprctl keyword monitor eDP-1,1920x1080@60.00hz, 0x0, 1
+	hyprctl keyword monitor eDP-1,1920x1080@60.00hz, 0x0, 1.25
 fi
 
 if [[ "$1" == off ]] ; then
 	hyprctl keyword decoration:blur:enabled true
 	hyprctl keyword decoration:shadow:enabled true
 	hyprctl keyword animations:enabled true
-	hyprctl keyword monitor eDP-1,1920x1080@165.01hz, 0x0, 1
+	hyprctl keyword monitor eDP-1,1920x1080@165.01hz, 0x0, 1.25
 fi
